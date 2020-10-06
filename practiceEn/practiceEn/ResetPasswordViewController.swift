da439c5907ebff7fcb440afc967e638b61ce6211
Chen-ZhaoJ <abc205977@gmail.com>
2020-10-06 16:05:05 +0800

    加入重設密碼頁面

---
+++ practiceEn/practiceEn/ResetPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResetPasswordViewController.swift
+//  practiceEn
+//
+//  Created by Ariel on 2020/10/6.
+//  Copyright © 2020 zencher. All rights reserved.
+//
+
+import UIKit
+
+class ResetPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
